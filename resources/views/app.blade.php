7369c6feb35b03579bb619c2bed186747b2509a6
Anélie <anelie.coustenoble@gmail.com>
2025-11-12 08:51:13 +0100

    modifs de jsp quoi mais tkt

---
+++ resources/views/app.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Memories 4Ever</title>
+  <link rel="stylesheet" href="style.css" />
+
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+</head>
+
+<body>
+<header>
+    <a href="index.html" alt="logo">Memories 4EVer</a>
+        <nav>
+            <li><a href="album.php">Album</a></li>
+            <li><a href="ajout.php">Ajouter des photos</a></li>
+        </nav>
+</header>
+
+@yield("content")
+
+<footer>
+    <a href="index.html" alt="logo">Memories 4EVer</a>
+    <p>© 2025 Memories 4Ever</p>
+</footer>
