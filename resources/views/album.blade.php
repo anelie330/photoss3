9f82fbb7035765d1f7334873330d69a9f5cd72cd
Lola Delannoy <lola.delannoy18@gmail.com>
2025-12-10 08:46:53 +0100

    css supprimé oupsiiiii

--- resources/views/album.blade.php
+++ resources/views/album.blade.php
@@ -1,18 +1,20 @@
 @extends("app")
 @section("content")        
 @foreach($photos as $p)
+<link rel="stylesheet" href="../css/style.css" />
 <div id ="photos">
     <h3>{{$p->titre}}</h3>
     <img class="photo" src ="{{$p->url}}" alt ="{{$p->titre}}" />
     <h3>{{$p->tags}}</h3>
 
-    <script>
-        document.querySelectorAll('.photos').forEach(function(img) {
+    <<script>
+    document.querySelectorAll('.photo').forEach(function(img) {
         img.addEventListener('click', function() {
-        img.classList.toggle('zoomed');
+            img.classList.toggle('zoomed');
+        });
     });
-});
     </script>
+
 </div>
 @endforeach
 @endsection
